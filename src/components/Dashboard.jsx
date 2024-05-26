bbe1e9bbe5446678c24004449d66a5acc133e9b9
Thiago Viana - tvm.dev2015 <tvm.dev2015@gmail.com>
2024-05-26 17:50:15 -0300

    Divs Totals, Transactions and BuyerProfile done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
+import DashboardStatsGrid from './DashboardStatsGrid'
+import TransitionChart from './TransactionsChart'
+import BuyerProfileChart from './BuyerProfileChart'
 export default function Dashboard() {
-    return <div>Dashboard</div>
+    return (
+        <div className="flex flex-col gap-4">
+            <DashboardStatsGrid />
+            <div className="flex gap-4 w-full ">
+                <TransitionChart />
+                <BuyerProfileChart />
+            </div>
+            <div className="flex flex-row gap-4 w-full "></div>
+        </div>
+    )
 }
